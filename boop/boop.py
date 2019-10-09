bf0bade8912c05a345d698ed819dca456edc54d6
Alex Pensinger <aplumafreak500@gmail.com>
2019-10-09 18:48:11 +0000

    Boop now stores user IDs instead of tags
    Boop now mentions the target, as a mention can be averted by passing the user ID of the target

--- boop/boop.py
+++ boop/boop.py
@@ -10,6 +10,8 @@ class Boop(BaseCog):
 		self.conf = Config.get_conf(self, identifier=0xb00b0001)
 		self.conf.register_user(boops = 0, booped_by = {})
 
+		# TODO: Per guild boop list
+
 	@commands.command()
 	@commands.cooldown(rate=3, per=10, type=commands.BucketType.user)
 	@commands.guild_only() # it's verified to work via DMs though
@@ -22,11 +24,22 @@ class Boop(BaseCog):
 		boops += 1
 		await self.conf.user(ctx.author).set_raw("boops", value = boops)
 		try:
-			booped_by_ = int(await self.conf.user(user).booped_by.get_raw(ctx.author))
+			booped_by_ = int(await self.conf.user(user).booped_by.get_raw(ctx.author.id))
 		except KeyError:
 			booped_by_ = 0
-		await self.conf.user(user).booped_by.set_raw(ctx.author, value=booped_by_ + 1)
-		sent_msg = "{0} has booped {1}".format(ctx.author.display_name, user.display_name)
+		# The old method just used ctx.author, support it for now
+		try:
+			if booped_by_ == 0:
+				booped_by_ = int(await self.conf.user(user).booped_by.get_raw(ctx.author))
+		except KeyError:
+			booped_by_ = 0
+		# Delete the old entry if it's present
+		try:
+			await self.conf.user(user).booped_by.clear_raw(ctx.author)
+		except:
+			pass
+		await self.conf.user(user).booped_by.set_raw(ctx.author.id, value=booped_by_ + 1)
+		sent_msg = "{0} has booped {1}".format(ctx.author.display_name, user.mention)
 		if message is not None:
 			sent_msg += " with the message: {0}!".format(message)
 		else:
@@ -44,8 +57,16 @@ class Boop(BaseCog):
 			msg = "{0}'s booped victims: \n".format(user.display_name)
 			total = 0
 			for i in boops:
+				try:
+					boop_user = discord.utils.get(self.bot.get_all_members(), id = int(i))
+				except ValueError: # for str -> int errors
+					boop_user = None
+				if boop_user == None:
+					username = i
+				else:
+					username = boop_user.display_name
 				times = int(boops.get(i))
-				msg += "{0} ({1} times)\n".format(i, times)
+				msg += "{0} ({1} times)\n".format(username, times)
 				total += times
 		except KeyError: # not booped yet
 			await ctx.send("This user hasn't been booped yet!")
